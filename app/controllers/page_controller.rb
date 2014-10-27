ddadeaaa5ca2aa54a570c9fce2ca912f2c9c0f83
uchsarath <sarath7khmer@gmail.com>
2014-10-28 00:25:51 +0700

    restructure assets & views

---
+++ app/controllers/page_controller.rb
@@ -0,0 +1,9 @@
+class PageController extends ApplicationController
+  def challenge
+    # ... any action related to the page Challenge
+  end
+
+  def cause
+    # ... action related to page Cause
+  end
+end
