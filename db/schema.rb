e22ad02e962b7b6bf7f6b25c885ab1665f2d5409
Chuy Thong <ichuythong@gmail.com>
2014-10-31 11:29:45 +0700

    remove file database.yml, sqlite3 and extension 'plpgsql', add database.yml.example.

--- db/schema.rb
+++ db/schema.rb
@@ -14,7 +14,7 @@
 ActiveRecord::Schema.define(version: 20141027154221) do
 
   # These are extensions that must be enabled in order to support this database
-  enable_extension "plpgsql"
+  # enable_extension "plpgsql"
 
   create_table "donators", force: true do |t|
     t.string   "name"
